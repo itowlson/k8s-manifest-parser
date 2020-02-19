3d6077838ef58fa6ac737116b870507f1d284470
itowlson <github@hestia.cc>
2020-02-19 19:23:40 +1300

    Notes to self

--- ts/src/convenience.ts
+++ ts/src/convenience.ts
@@ -1,5 +1,31 @@
 import * as model from './model';
 
+// TODO: I feel I want to make this safer, and that I could do
+// so without all the proxy nonsense, by building up a structure
+// of the form:
+//
+// interface Node {
+//    [key: string | number]: Node;  // string key is error if not on map, num key is error if not on array
+//    value: string | number | boolean;  // error on other types
+//    ^^ or asString / asNumber / asBoolean / asArray / asMap each giving suitable typechecks, so you could do a.asNumber() < b.asNumber() and be assured that the operation existed
+//    range: model.Range;  // error on other types
+//    keyRange: model.Range;  // error if parent is not a map
+// }
+
+// TODO: things that don't exist should still return values so that the user can
+// call an exists() or isPresent() method
+
+// Another idea: navigation by methods which is wordier but would allow you to
+// set expectations and could be more strongly typed:
+//
+// ast.string('apiVersion')
+// ast.map('spec').array('containers').map(0).string(image).text()  // expects things to exist and throws if not
+// ast.map('spec').map$('labels').string$('my-label').text()  // allows .labels and .my-label not to exist
+//
+// Not sure how useful the 'throw if not exist' version is; surely it has to throw at the .text()
+// stage anyway so if the user cares they can just do an exists() on the return of string().
+// But the method style has something going for it maybe.
+
 export function convenientify(impl: model.ResourceParse): any {
     const handler = {
         has: (target: object, p: PropertyKey) => {
